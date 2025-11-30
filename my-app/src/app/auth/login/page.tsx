880b819332004f6a03f7365bdcac7c473ce061a7
MostafaJR <160778812+MostafaJR@users.noreply.github.com>
2025-11-30 16:33:10 +0000

    .

--- my-app/src/app/auth/login/page.tsx
+++ my-app/src/app/auth/login/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import LoginForm from "./LoginForm";
 
-export default function LoginPage(){
-  return <LoginForm />
+export default function LoginPage() {
+  return (
+    <Suspense fallback={<div className="w-full h-screen flex justify-center items-center">Loading...</div>}>
+      <LoginForm />
+    </Suspense>
+  );
 }
